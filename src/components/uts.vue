d412edb2c442d9378fcb24270c3fd0a20e60556e
Ari Kristian <keajaibandunia321@gmail.com>
2025-05-07 22:09:36 +0700

    Membuat Tampilan agar bisa menampilkan Daftar kegiatan yang sudah di tambahkan

--- src/components/uts.vue
+++ src/components/uts.vue
@@ -1,10 +1,33 @@
 <script setup>
+  import {ref ,computed, watch, onMounted} from 'vue'
+
+  const tombol = ref(null)
+
+  const teks = ref('')
+
+  const lists = ref([
+    {text : "Olahraga", status : true},
+    {text : "Makan" , status : true},
+    {text : "Tidur", status : false}
+  ])
+
 
 </script>
 
 <template>
 
+<input type="text" v-model="teks">
+<button v-on:click="tambahkan" :disabled="teks == ''">submit</button>
+
+<h2>Daftar Kegiatan</h2>
+<ul>
+  <li v-for="list in lists"> {{ list.id }}. {{ list.text }} : {{ list.status }}
+  </li>
+</ul>
+
+
 </template>
+
 
 <style scoped>
 
